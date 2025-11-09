7a862c3436bbeb616529db5ba6a2ccec3f57d7cc
Oreva-J <orevaawusi@gmail.com>
2025-11-09 15:03:56 +0100

    deploy fixs v5

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,19 +1,21 @@
-import express from "express"
+import express from "express";
 import router from "./routes/summerizerRoute.js";
 
-
-
-const app = express()
-const port = 4000
+const app = express();
+const port = 4000;
 
 app.use(router);
 
-app.get("/api",(req, res)=>{
-    res.json({message: "it workedd"})
-})
+app.get("/api", (req, res) => {
+  res.json({ message: "it workedd" });
+});
 
-app.listen(port, ()=>{
-    console.log(`Server listen to Port: ${port}`)
-})
+// For local development
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => {
+    console.log(`API running on port ${PORT}`);
+  });
+}
 
-export default app
+export default app;
